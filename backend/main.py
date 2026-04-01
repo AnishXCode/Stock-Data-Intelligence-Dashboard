126d293226216607c0061668e7793cd29d530a5c
AnishXCode <anishharshaj@gmail.com>
2026-04-02 01:37:32 +0530

    Added CORS URL for backend

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "*"],
+    allow_origins=["https://stock-frontend-0m9q.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
